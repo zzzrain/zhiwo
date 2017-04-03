00df4b5df19231e258f1a41542fe2f08337365f2
zero <1594087542@qq.com>
2017-04-03 21:17:14 +0800

    登录

--- src/js/register.js
+++ src/js/register.js
@@ -1,7 +1,23 @@
 require(['config'],function(){
 	require(['jquery','common'],function(){
 		$(()=>{
-			$('form').on('click','#agree',function(){
+			// 验证码函数
+			function verify(res){
+				// 随机生成一个4位验证码（包含字母）
+				var str = 'abcdefghijklmnopqrstuvwxyz0123456789';
+				var num = '';
+				for(var i=0;i<4;i++){
+					var index = parseInt(Math.random()*str.length); //不可能大于36
+					num += str[index]
+				}
+				res.html(num.toUpperCase());
+			}
+
+			verify($('#verify'));
+
+			$('form').on('click','#change',function(){
+				verify();
+			}).on('click','#agree',function(){
 				if(!/^1[34578]\d{9}$/.test($('#phone').val())){
 					alert('手机格式有误，请重新输入');
 					return false;
@@ -14,9 +30,12 @@ require(['config'],function(){
 					alert('两次输入的密码不匹配');
 					return false;
 				}
+				if($('.verify input').val() != $('#verify').html()){
+					alert('验证码有误，请重新输入');
+					return false;
+				}
 				$.post('../php/register.php', {phone:$('#phone').val(),password:$('#password').val()})
-			})
-
+			});
 
 			/*$('form').on('blur','#phone',function(){
 				if(!/^1[34578]\d{9}$/.test($(this).val())){
--- src/php/register.php
+++ src/php/register.php
@@ -1,5 +1,42 @@
-<?php
-	$phone = $_POST['phone'];
-	$password = $_POST['password'];
-	echo $phone;
+<?php	
+	$tel = $_POST['phone'];
+	$psw = $_POST['password'];
+	 
+    //准备好数据连接对象
+    $con = new mysqli('localhost','root','root','zhiwo'); 
+  
+    //获取所有用户信息
+    $sql = 'select * from register';
+    $res = $con->query($sql);
+    // echo $sql;
+
+    //获取的用户信息与当前注册信息进行比较
+    $bool = true;
+    //数据库查询结果的长度 $res->num_rows
+    if($res->num_rows>0){
+        //fetch_assoc() 执行第一次返回 第一条数据 执行第二次的时候返回第二数据
+        //$row 当前获取到的每一行数据
+        while ($row = $res->fetch_assoc()) {
+            //判断用户是否注册
+            if($row['phone'] == $tel){
+                $bool = false; 
+            }
+        }
+    }
+
+    //通过判断是否插入新用户
+    if($bool){
+        //注册用户的sql语句
+        $sql = 'insert into register(phone,password) values("'.$tel.'","'.$psw.'")';
+        if($con->query($sql)){
+            echo "注册成功";
+        }else{
+            echo "注册失败";
+        }
+    }else{
+        echo '用户已注册';
+    }
+
+    //关闭数据库
+    $con->close();
 ?>
